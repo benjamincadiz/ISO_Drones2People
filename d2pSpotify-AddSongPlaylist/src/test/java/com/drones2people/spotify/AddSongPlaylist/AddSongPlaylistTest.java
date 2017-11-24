da3203a55dd69764f2783bea4a03c15f89dc4e31
bersus96 <32294171+bersus96@users.noreply.github.com>
2017-11-24 23:29:50 +0100

    IT.20 - Añadir Cancion a PlayList

---
+++ d2pSpotify-AddSongPlaylist/src/test/java/com/drones2people/spotify/AddSongPlaylist/AddSongPlaylistTest.java
@@ -0,0 +1,38 @@
+package com.drones2people.spotify.AddSongPlaylist;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple AddSongPlaylist.
+ */
+public class AddSongPlaylistTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AddSongPlaylistTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AddSongPlaylistTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
