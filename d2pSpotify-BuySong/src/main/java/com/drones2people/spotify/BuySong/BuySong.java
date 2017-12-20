5ec08d26609e1188d5dc098f80fcb233928064b0
bersus96 <32294171+bersus96@users.noreply.github.com>
2017-12-20 13:43:21 +0100

    Update Test BuySong.2

--- d2pSpotify-BuySong/src/main/java/com/drones2people/spotify/BuySong/BuySong.java
+++ d2pSpotify-BuySong/src/main/java/com/drones2people/spotify/BuySong/BuySong.java
@@ -21,43 +21,7 @@ public class BuySong {
     GestorCanciones gestorCanciones;
     GestorUsuarios gestorUsuarios;
 
-    public Usuario selectUser_byName (String Nombre) throws SQLException{
-                Usuario usuario = new Usuario();
-                String query = "SELECT * FROM Usuario WHERE Nombre = ?";
 
-                preparedStatement = agente.getConnection().prepareStatement(query);
-                preparedStatement.setString(1, Nombre);
-                ResultSet resultSet = preparedStatement.executeQuery();
-
-                while (resultSet.next()) {
-                        usuario.setDNI(resultSet.getInt("DNI"));
-                        usuario.setNombre(resultSet.getString("Nombre"));
-                        usuario.setApellidos(resultSet.getString("Apellidos"));
-                        usuario.setEmail(resultSet.getString("email"));
-                        usuario.setPassword(resultSet.getString("password"));
-                        usuario.setTelefono(resultSet.getString("Telefono"));
-                        usuario.setIs_admin(resultSet.getBoolean("isAdmin"));
-                        usuario.setIs_artist(resultSet.getBoolean("isArtist"));
-                    }
-                return usuario;
-            }
-    public Cancion selectSong(String name, int artist) throws SQLException{
-                Cancion song = new Cancion();
-                String query = "SELECT * FROM Cancion WHERE nombre = ? and artista = ?";
-                preparedStatement = agente.getConnection().prepareStatement(query);
-                preparedStatement.setString(1, name);
-                preparedStatement.setInt(2, artist);
-                ResultSet resultSet = preparedStatement.executeQuery();
-
-                while (resultSet.next()) {
-                    song.setNombre(resultSet.getString("Nombre"));
-                    song.setArtista(resultSet.getInt("Artista"));
-                    song.setDate(resultSet.getDate("fecha"));
-                    song.setAlbum(resultSet.getString("album"));
-                    song.setDuracion(resultSet.getDouble("Duracion"));
-                }
-                return song;
-          }
     public boolean buySong(Cancion cancion) throws SQLException{
         try {
             agente = Agente.getAgente();
